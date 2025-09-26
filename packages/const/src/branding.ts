417346cfe4208e62fab81e7c8019aa4f3966ea50
Brandon Klindt <brandon.klindt@icloud.com>
2025-09-26 17:34:01 +0200

    Update branding.ts as per Henni requirements

--- packages/const/src/branding.ts
+++ packages/const/src/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'AskAfrica Cloud';
+export const LOBE_CHAT_CLOUD = 'ASK AFRICA Cloud';
 
-export const BRANDING_NAME = 'AskAfrica Chat';
+export const BRANDING_NAME = 'ASK AFRICA Chat';
 export const BRANDING_LOGO_URL = 'https://res.cloudinary.com/dadgglcaq/image/upload/t_Favicon/v1747746280/Ask_Africa_logo_website_2_d7sams.png';
 
-export const ORG_NAME = 'AskAfrica Chat';
+export const ORG_NAME = 'ASK AFRICA Chat';
 
 export const BRANDING_URL = {
   help: undefined,
@@ -16,14 +16,14 @@ export const BRANDING_URL = {
 };
 
 export const SOCIAL_URL = {
-  discord: 'https://discord.gg/AYFPHvv2jT',
+  discord: 'https://askafrika.co.za',
   github: 'https://github.com/dissemblir',
-  medium: 'https://medium.com/@lobehub',
-  x: 'https://x.com/lobehub',
-  youtube: 'https://www.youtube.com/@lobehub',
+  medium: 'https://askafrika.co.za',
+  x: 'https://askafrika.co.za',
+  youtube: 'https://askafrika.co.za',
 };
 
 export const BRANDING_EMAIL = {
-  business: 'hello@lobehub.com',
-  support: 'support@lobehub.com',
+  business: 'connect@askafrica.co.za',
+  support: 'nwasupport@askafrica.co.za',
 };
