f6b2c6e8ac851091633c99e4b461b50b0ae18992
Brandon Klindt <brandon.klindt@icloud.com>
2025-09-26 16:24:00 +0200

    Update branding.ts

--- packages/const/src/branding.ts
+++ packages/const/src/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'AskAfrica Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = 'AskAfrica Chat';
+export const BRANDING_LOGO_URL = 'https://ik.imagekit.io/qcvroy8xpd/downloads/Favicon_eoh4ck.png';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'AskAfrica Chat';
 
 export const BRANDING_URL = {
   help: undefined,
@@ -17,7 +17,7 @@ export const BRANDING_URL = {
 
 export const SOCIAL_URL = {
   discord: 'https://discord.gg/AYFPHvv2jT',
-  github: 'https://github.com/lobehub',
+  github: 'https://github.com/dissemblir',
   medium: 'https://medium.com/@lobehub',
   x: 'https://x.com/lobehub',
   youtube: 'https://www.youtube.com/@lobehub',
